b22e6aa607fde30b939dc4bf793aa466db2d5b63
Andreas Weber <andreas.weber@microfocus.com>
2021-02-21 12:46:23 +0100

    better node install

--- node.sh
+++ node.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -ex
 
-echo "Installing NodeJs ..."
+echo "Installing NodeJs and build tools ..."
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt install -y nodejs
 
-# You may also need development tools to build native addons:
-#sudo apt-get install gcc g++ make
+echo "Installing build tools for native modules ..."
+sudo apt install -y gcc g++ make
+
+echo "Installing global node_modules ..."
+sudo npm install grunt eslint -g
