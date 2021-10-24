10fad5f64fc21f4f41d5f6372f0c78f5fcd4d057
hulmgulm <github@hulmgulm.de>
2021-10-24 18:34:22 +0200

    Updated docker install script

--- docker.sh
+++ docker.sh
@@ -2,16 +2,23 @@
 set -ex
 
 echo "Installing docker ..."
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo apt update
-sudo apt install -y docker-ce
+sudo apt install --no-install-recommends  -y apt-transport-https ca-certificates curl gnupg2
+
+if [ -f /etc/os-release ]; then
+  # On Debian or Ubuntu temporarily set some OS-specific variables
+  source /etc/os-release
+  # Make sure that apt will trust the repo
+  curl -fsSL https://download.docker.com/linux/${ID}/gpg | sudo apt-key add -
+  echo "deb [arch=amd64] https://download.docker.com/linux/${ID} ${VERSION_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/docker.list
+  sudo apt update
+fi
+
+sudo apt install -y docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker ${USER}
-echo 'hulmi ALL=(ALL) NOPASSWD: /usr/bin/dockerd' | sudo EDITOR='tee -a' visudo
+echo '${USER} ALL=(ALL) NOPASSWD: /usr/bin/dockerd' | sudo EDITOR='tee -a' visudo
 
 echo "Installing docker-compose ..."
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 echo "Configuring profile ..."
