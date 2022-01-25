c8943edfad49b6c885f138febab1b0c1f66a9b95
Andreas Weber <andreas.weber@microfocus.com>
2022-01-25 09:09:04 +0100

    Make sure, certificate directories do exist

--- docker/nginx/nginx.sh
+++ docker/nginx/nginx.sh
@@ -2,6 +2,8 @@
 set -ex
 
 # create TLS certificate
+mkdir -p /etc/ssl/private
+mkdir -p /etc/ssl/certs/
 openssl req -x509 -nodes -days 365 -subj "/C=CA/ST=QC/O=Company, Inc./CN=${HOSTNAME}" -addext "subjectAltName=DNS:${HOSTNAME},DNS:localhost" -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt;
 
 # replace hostname in nginx config files with real host name
