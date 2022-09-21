5fd6605d7cd334faab287be7f380638ceca3d8d9
hulmgulm <hulmgulm@users.noreply.github.com>
2022-09-21 08:55:41 +0200

    docker-compose is now a docker plugin

--- docker/docker-up.cmd
+++ docker/docker-up.cmd
@@ -3,4 +3,4 @@ set /p HOSTNAME= < tmpFile
 wsl hostname -I > tmpFile
 set /p WSL_IP= < tmpFile
 del tmpFile
-docker-compose up --force-recreate --build -d
+docker compose up --force-recreate --build -d
